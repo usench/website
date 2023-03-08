4f300c7bc775aa8ad4a0576827b540e9bff34d04
mokun <kun@fit2cloud.com>
2023-03-08 15:25:39 +0800

    refactor: 调整页面

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="footer-container">
-    <div class="content">
-      <div class="name">FIT2CLOUD 飞致云</div>
-      <div class="line"/>
-      <div class="footer">
-        <div class="info">
-          <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener">浙ICP备14038283号-5</a>
-        </div>
-
+  <div class="footer-page">
+    <div class="footer-container">
+      <div class="navigation">
+        <div class="name">FIT2CLOUD 飞致云</div>
         <div class="links">
-          <a href="https://1panel.cn/docs" target="_blank" rel="noopener">文档</a>
-          <a href="https://github.com/1Panel-dev/1Panel" target="_blank" rel="noopener">GitHub</a>
+          <a href="https://1panel.cn/docs" target="_blank">文档</a>
+          <a href="https://github.com/1Panel-dev/1Panel" target="_blank">GitHub</a>
           <a href="https://www.fit2cloud.com/about/index.html" target="_blank">关于我们</a>
         </div>
       </div>
+      <div class="line"/>
+      <div class="license">
+        <p class="info">
+          <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备14038283号-5</a>
+        </p>
+        <p>
+          <a href="https://www.fit2cloud.com/legal/tos.html" target="_blank">使用条款</a>
+          |
+          <a href="https://www.fit2cloud.com/legal/privacy-policy.html" target="_blank">隐私政策</a>
+          |
+          <a href="https://www.fit2cloud.com/legal/licenses.html" target="_blank">社区软件许可协议</a>
+        </p>
+        <p>
+          违法网站请勿向我司工作人员发起任何形式的人工服务请求，严禁使用 1Panel 面板从事任何非法活动
+        </p>
+      </div>
     </div>
-
   </div>
 </template>
 
@@ -26,53 +36,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.footer-container {
+.footer-page {
   background: #161618;
   padding: 64px 0;
   @media only screen and (max-width: 833px) {
     padding: 32px 0;
   }
 
-  .content {
+  .footer-container {
     max-width: 1128px;
     margin: 0 auto;
-    color: #FFFFFF;
 
-    .name {
-      color: #f8f9fc;
-      font-size: 20px;
-      letter-spacing: 1.5px;
-      font-weight: 800;
-      @media only screen and (max-width: 833px) {
-        text-align: center;
-      }
-    }
-
-    img {
-      height: 32px;
-    }
-
-    .line {
-      border-top: 1px solid rgba(243, 229, 200, 0.26);
-      margin: 24px 0;
-    }
-
-    .footer {
+    .navigation {
+      color: #FFFFFF;
       display: flex;
       justify-content: space-between;
-      align-items: center;
-      font-size: 14px;
-      line-height: 24px;
-
       @media only screen and (max-width: 833px) {
-        flex-direction: column;
-        gap: 40px;
+        justify-content: center;
       }
 
-      .info {
-        align-items: center;
-        display: flex;
-        gap: 20px;
+      .name {
+        color: #f8f9fc;
+        font-size: 20px;
+        letter-spacing: 1.5px;
+        font-weight: 800;
+        @media only screen and (max-width: 833px) {
+          text-align: center;
+        }
       }
 
       .links {
@@ -83,6 +73,29 @@ export default {
         @media only screen and (max-width: 833px) {
           display: none;
         }
+      }
+    }
+
+    .line {
+      border-top: 1px solid rgba(243, 229, 200, 0.26);
+      margin: 24px 0;
+    }
+
+    .license {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+
+      p {
+        color: #adb5bd;
+        font-weight: 300;
+        font-family: "Source Han Sans SC", "Noto Sans CJK SC", "Source Han Sans CN", "Noto Sans SC", "Source Han Sans TC", "Noto Sans CJK TC", sans-serif;
+        text-align: center;
+
+        padding: 0 12px;
+        margin: 0;
       }
     }
 
